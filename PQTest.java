9192e5819ab7b80ed5ac54735bbcff3ec49f8d1d
JH0123 <80099290+JH0123@users.noreply.github.com>
2021-04-15 18:37:54 +0900

    Add files via upload

---
+++ PQTest.java
@@ -0,0 +1,30 @@
+package ch15;
+
+import java.util.*;
+
+public class PQTest {
+	public static void main(String args[]) {
+		test1();
+	}
+	public static void test1() {
+		Queue<String> q = new PriorityQueue<>();
+		
+		q.offer("PineApple"); //q.add();
+		q.offer("Banana");
+		q.offer("Carrot");
+		q.offer("Cherry");
+		q.offer("Strawberry");
+		
+		//엿보기, 안출은 아니야~
+		System.out.println(q.peek()); //q.element()
+		System.out.println(q.peek());
+		System.out.println(q.peek());
+		System.out.println("###############");
+		
+		while(q.size() > 0) {
+			System.out.println(q.poll()); //q.remove()
+			
+		}
+		
+	}
+}
