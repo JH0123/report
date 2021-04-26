7cce0edd2eedb519d85c9142bda580148dd22ba5
JH0123 <80099290+JH0123@users.noreply.github.com>
2021-04-26 14:20:38 +0900

    Add files via upload

---
+++ StackTest.java
@@ -0,0 +1,26 @@
+package ch15;
+
+import java.util.*;
+
+public class StackTest {
+	public static void main(String args[]) {
+		/*
+		 * Stack : LIFP(Last In First Out
+		 * Generic : 클래스 내부에서 사용할 데이터 타입이 정해진 것이 아니고
+		 * 그 클래스의 객체를 생성할 떄 데이터 타입을 결정할 수 있도록 ,
+		 * 사용할 테이터 타입을 파라미터로 받아서 객체로 생성
+		 * 타입 파라미터(Type Parameter)
+		 */
+		Stack<Integer> stack = new Stack<>();
+		for(int i =0; i<10; i++) {
+			stack.push(i+1);
+		}
+		System.out.println(stack);
+		//10,9,8,7,6,5,4,3,2,1
+		while(!stack.isEmpty()) {
+			Integer val = stack.pop();
+			System.out.println(val);
+			
+		}
+	}
+}
